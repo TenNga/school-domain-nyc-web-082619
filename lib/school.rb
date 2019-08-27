49e240f94b3cf60593efe454b0ecc7d4f9498ce8
Kevin Karma <kevinkarma55@gmail.com>
2019-08-27 20:49:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,8 @@ class School
   
   def add_student(name,grade)
     @roster[grade] = [ ]
-    
-    @roster[grade] << namebinding.pry
+    @roster[grade] << name
+    binding.pry
   end
   
   def grade(grade)
