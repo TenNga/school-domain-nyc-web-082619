9e46ddd1d9ccaebdcfb623769246c9a1235f89cf
Kevin Karma <kevinkarma55@gmail.com>
2019-08-27 20:31:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  def initialize (sName)
+    @schoolName = sName
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+  def add_student(name,grade)
+    @roster[grade] = []
+    @roster[grade] << grade
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster
+  end
+end
